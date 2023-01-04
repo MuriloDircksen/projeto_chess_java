4303dcabbc9a0d9d74305e298f7445f421afaa8a
Murilod <murilo.dircksen@gmail.com>
2023-01-04 18:51:03 -0300

    mudando de jogador em cada turno

--- src/application/UI.java
+++ src/application/UI.java
@@ -1,6 +1,7 @@
 package application;
 
 
+import chess.ChessMatch;
 import chess.ChessPiece;
 import chess.ChessPosition;
 import chess.Color;
@@ -32,8 +33,9 @@ public class UI {
     public static final String ANSI_CYAN_BACKGROUND = "\u001B[46m";
     public static final String ANSI_WHITE_BACKGROUND = "\u001B[47m";
 
-    public static void clearScreen(){
-        System.out.print("\033[h\033[2J");
+    // https://stackoverflow.com/questions/2979383/java-clear-the-console
+    public static void clearScreen() {
+        System.out.print("\033[H\033[2J");
         System.out.flush();
     }
     public static ChessPosition readChessPosition(Scanner sc){
@@ -46,6 +48,13 @@ public class UI {
         catch (RuntimeException e){
             throw new InputMismatchException("Erro lendo a posição da peça, valores válidos de a1 até h8!");
         }
+    }
+
+    public static void printMatch(ChessMatch chessMatch){
+        printBoard(chessMatch.getPieces());
+        System.out.println();
+        System.out.println("Turno: "+ chessMatch.getTurn());
+        System.out.println("Esperando jogador: "+chessMatch.getCurrentPlayer());
     }
     public static  void printBoard(ChessPiece[][] pieces){
         for(int i =0; i<pieces.length; i++){
--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -8,10 +8,21 @@ import chess.pieces.Rook;
 
 public class ChessMatch {
     private Board board;
+    private int turn;
+    private Color currentPlayer;
 
     public ChessMatch(){//criando nova partida
         board = new Board(8,8);//criando tabuleiro
+        turn = 1;
+        currentPlayer = Color.WHITE;
         initialSetup(); //criando posições iniciais
+    }
+
+    public int getTurn(){
+        return turn;
+    }
+    public Color getCurrentPlayer(){
+        return currentPlayer;
     }
     public ChessPiece[][] getPieces(){ //pegando as peças e suas posições
         ChessPiece[][] mat = new ChessPiece[board.getRows()][board.getColumns()];
@@ -35,6 +46,7 @@ public class ChessMatch {
         validateSourcePosition(source);
         validateTargetPosition(source, target);
         Piece capturedPiece = makeMove(source, target);
+        nextTurn();
         return (ChessPiece)capturedPiece;
     }
 
@@ -42,6 +54,10 @@ public class ChessMatch {
         if(!board.thereIsPiece(position)){
             throw new ChessException("Não tem peça no posição indicada!");
 
+        }
+
+        if(currentPlayer!=((ChessPiece)board.piece(position)).getColor()){
+            throw new ChessException("A peça escolhida não é sua!");
         }
         if(!board.piece(position).isThereAnyPossibleMove()){
             throw new ChessException("Não existe movimentos para peça escolhida");
@@ -59,6 +75,11 @@ public class ChessMatch {
         Piece capturedPiece = board.removePiece(target);
         board.placePiece(p, target);
         return capturedPiece;
+    }
+
+    private void nextTurn(){
+        turn++;
+        currentPlayer = (currentPlayer==Color.WHITE) ? Color.BLACK:Color.WHITE;
     }
 
     private void placeNewPiece(char column, int row, ChessPiece piece){
